b7d8dc5d75b185cf1094cd7f1cae6f7f20997ca0
Milinda Rajapakshe <milinda@codegen.net>
2020-03-10 22:53:37 +0530

    folium map location

--- folium/maps.py
+++ folium/maps.py
@@ -1,4 +1,8 @@
 import folium
 
-mapA = folium.Map([37.7510, -97.8220], zoom_start=5)
+mapA = folium.Map([7.872, 80.401], zoom_start=8, title="Test Map")
+fg = folium.FeatureGroup(name="Volcanoes")
+fg.add_child(folium.Marker(location=[7.872, 80.401], popup="Home", icon=folium.Icon(color="red")))
+
+mapA.add_child(fg)
 mapA.save("test.html")
